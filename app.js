33f0d41d3cdc19df9efc72ca6e017525f3f4f4f7
CadillacJenkins <walter.k.jenkins@gmail.com>
2019-10-21 22:37:23 -0500

    redid styling for inputs / addData Function

--- app.js
+++ app.js
@@ -5,8 +5,58 @@ todo: get timer to start
 todo: add timer with name
 !
 */
-const stopBut = 'img/stop.png';
-const playBut = 'img/play.png';
+let id = 0;
+
+// START OF CONSTANT VARIABLES
+window.onload = ()=>{
+
+}
+
+const getData = () =>{
+    let timerInput = document.getElementById('title').value;
+    let typeInput = document.getElementById('timer-type').value;
+    return{
+        id,
+        title: timerInput,
+        type: typeInput
+    }
+}
+const addData = ()=>{
+    let title = getData().title;
+    let type = getData().type;
+
+    const div = 
+        `<div id="Timer-${id}" class="timer-list">
+            <div class="timer-list">
+                <h2 class="timer-title" id="timer-title-1">${title}</h2>
+        
+                <svg height="100" width="20"><line x1="10" y1="35" x2="10" y2="65" style="stroke:black;stroke-width:2" /></svg>  
+                    
+                <h2 class="timer-type" id="timer-type-${id}">${type}</h2>
+
+                <svg height="100" width="20"><line x1="10" y1="35" x2="10" y2="65" style="stroke:black;stroke-width:2" /></svg>  
+                
+                <h2 class="timer-count" class="timer-count-${id}">00:04:20</h2>
+                
+                <svg height="100" width="20"><line x1="10" y1="35" x2="10" y2="65" style="stroke:black;stroke-width:2" /></svg>
+
+                <button class="button list" onclick="playClick(${id})"><img id='play-control' src="img/play.svg"></button>
+
+                <svg height="100" width="20"><line x1="10" y1="35" x2="10" y2="65" style="stroke:black;stroke-width:2" /></svg>
+
+                <button class="button" onclick="deleteClick(${id})"><img id='delete' src="img/delete.svg"></button>    
+
+            </div>
+        </div>`;
+
+        document.querySelector('#active-timers').outerHTML+= div;
+
+    id++;
+}
+
+
+
+// START OF TIMER LIST SECTINO
 let play = 0;
 
 const playClick = () => {
